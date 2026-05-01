8d79fd3db1029a09058b6403a1c7c5a5871fa209
anna-bitsafe <anna@bitsafe.finance>
2026-05-01 13:28:50 -0400

    Update rates.js

--- api/rates.js
+++ api/rates.js
@@ -1,4 +1,4 @@
-js
+
 export default async function handler(req, res) {
   const { start, end } = req.query;
   const r = await fetch(`https://api.frankfurter.dev/v1/${start}..${end}?from=USD&to=EUR,GBP`);
